00318f5ff729d3a22b34e0fde0d0e922f74e9ccc
vagrant <vagrant@vagrant.vm>
2018-06-15 18:55:54 +0000

    pig_latin.py doctests

--- pig_latin.py
+++ pig_latin.py
@@ -1,3 +1,21 @@
+"""Turn a phrase into Pig Latin.
+
+This takes a space-separated phrase and returns it in Pig Latin.
+
+Rules:
+
+1. If the word begins with a consonant (not a, e, i, o, u),
+   move the first letter to the end and add 'ay'
+
+2. If the word begins with a vowel, add 'yay' to the end
+
+For example:
+
+    >>> pig_latin('hello awesome programmer')
+    'ellohay awesomeyay rogrammerpay'
+
+"""
+
 def pig_latin(phrase):
     """Turn a phrase into pig latin.
 
@@ -23,4 +41,10 @@ def pig_latin(phrase):
     		new_word_list.append(new_word)
     return ' '.join(new_word_list)
 
-print pig_latin('hello kitty')
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. REATGAY OBJAY!\n"
