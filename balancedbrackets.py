f0b7b92cd4fe8d8f2eae49f24949e70ddbb61fd5
Violet Yeh <violetsyeh@gmail.com>
2018-06-30 13:51:21 -0700

    balanced brackets

---
+++ balancedbrackets.py
@@ -0,0 +1,79 @@
+"""Does a given string have balanced pairs of brackets?
+
+Given a string, return True or False depending on whether the string
+contains balanced (), {}, [], and/or <>.
+
+Many of the same test cases from Balance Parens apply to the expanded
+problem, with the caveat that they must check all types of brackets.
+
+These are fine::
+
+   >>> has_balanced_brackets("<ok>")
+   True
+
+   >>> has_balanced_brackets("<{ok}>")
+   True
+
+   >>> has_balanced_brackets("<[{(yay)}]>")
+   True
+
+These are invalid, since they have too many open brackets::
+
+   >>> has_balanced_brackets("(Oops!){")
+   False
+
+   >>> has_balanced_brackets("{[[This has too many open square brackets.]}")
+   False
+
+These are invalid, as they close brackets that weren't open::
+
+   >>> has_balanced_brackets(">")
+   False
+
+   >>> has_balanced_brackets("(This has {too many} ) closers. )")
+   False
+
+Here's a case where the number of brackets opened matches
+the number closed, but in the wrong order::
+
+    >>> has_balanced_brackets("<{Not Ok>}")
+    False
+
+If you receive a string with no brackets, consider it balanced::
+
+   >>> has_balanced_brackets("No brackets here!")
+   True
+
+"""
+
+
+def has_balanced_brackets(phrase):
+    """Does a given string have balanced pairs of brackets?
+
+    Given a string as input, return True or False depending on whether the
+    string contains balanced (), {}, [], and/or <>.
+    """
+
+    brack_dict = {')':'(', '}':'{', ']':'[', '>':'<'}
+    brack_list = []
+
+    for char in phrase:
+      if char in brack_dict.values():
+        brack_list.append(char)
+      elif char in brack_dict.keys(): 
+        if brack_list == []:
+          return False
+        elif brack_list[-1] == brack_dict[char]:
+          brack_list.pop()
+        else:
+          return False
+    return brack_list == []
+
+
+        
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. YOU CAUGHT ALL THE STRAY BRACKETS!\n"
