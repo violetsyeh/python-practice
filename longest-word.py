f4e10caff22c0c963326af86a1a8a7cafb5c3138
Violet Yeh <violetsyeh@gmail.com>
2018-07-10 13:36:39 -0700

    longest-word

---
+++ longest-word.py
@@ -0,0 +1,28 @@
+"""Return longest word in list of words.
+
+For example::
+
+    >>> find_longest_word(["hi", "hello"])
+    5
+
+    >>> find_longest_word(["Balloonicorn", "Hackbright"])
+    12
+
+"""
+
+
+def find_longest_word(words):
+    """Return longest word in list of words."""
+
+    max_word = 0
+
+    for word in words:
+    	if len(word) > max_word:
+    		max_word = len(word)
+    return max_word
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. YOU'RE THE MAX!\n")
