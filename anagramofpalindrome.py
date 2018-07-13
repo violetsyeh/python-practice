d765160af9c3af463aa40ce782b8a2c5159f3bbd
Violet Yeh <violetsyeh@gmail.com>
2018-07-12 17:36:55 -0700

    anagram of palindrome

---
+++ anagramofpalindrome.py
@@ -0,0 +1,52 @@
+"""Is the word an anagram of a palindrome?
+
+A palindrome is a word that reads the same forward and backwards
+(eg, "racecar", "tacocat"). An anagram is a rescrambling of a word
+(eg for "racecar", you could rescramble this as "arceace").
+
+Determine if the given word is a re-scrambling of a palindrome.
+The word will only contain lowercase letters, a-z.
+
+Examples::
+
+    >>> is_anagram_of_palindrome("a")
+    True
+
+    >>> is_anagram_of_palindrome("ab")
+    False
+
+    >>> is_anagram_of_palindrome("aab")
+    True
+
+    >>> is_anagram_of_palindrome("arceace")
+    True
+
+    >>> is_anagram_of_palindrome("arceaceb")
+    False
+
+"""
+
+
+def is_anagram_of_palindrome(word):
+    """Is the word an anagram of a palindrome?"""
+
+    seen = {}
+
+    for letter in word:
+        count = seen.get(letter, 0)
+        seen[letter] = count + 1
+
+    seen_odd = False
+    
+    for count in seen.values():
+        if count % 2 != 0:
+            if seen_odd:
+                return False
+            seen_odd = True
+    return True
+
+if __name__ == '__main__':
+    import doctest
+
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TEST PASSED. AWESOMESAUCE!\n")
