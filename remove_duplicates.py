670591dfbc20289dd90569176b3020827d04a6e1
Violet Yeh <violetsyeh@gmail.com>
2018-07-13 14:27:36 -0700

    remove duplicates

---
+++ remove_duplicates.py
@@ -0,0 +1,32 @@
+""" Remove duplicates in a list
+
+For example::
+
+    >>> remove_duplicates([6, 9, 7, 9, 2, 6, 0])
+    [6, 9, 7, 2, 0]
+
+    >>> remove_duplicates([])
+    []
+
+    >>> remove_duplicates([6, 9, 7])
+    [6, 9, 7]
+
+"""
+
+
+def remove_duplicates(items):
+    """Remove duplicates in the list items and return that list."""
+    lst = []
+
+    for num in items:
+        if num not in lst:
+            lst.append(num)
+    return lst
+    
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. GREAT WORK!\n")
+
+
+
