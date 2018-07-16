221867cc500438a5cfaae7b0f302d274d6581456
Violet Yeh <violetsyeh@gmail.com>
2018-07-16 13:13:37 -0700

    renamed variable for clarity

--- selection_sort.py
+++ selection_sort.py
@@ -15,14 +15,14 @@ def selection_sort(array):
 	"""Using selection sort, return the sorted array"""
 
 	for i in range(len(array)-1, 0, -1):
-		max_pos = 0
+		max_value = 0
 		for j in range(1, i + 1):
-			if array[j] > array[max_pos]:
-				max_pos = j
-		# if i != min_value:
-		temp = array[i]
-		array[i] = array[max_pos]
-		array[max_pos] = temp
+			if array[j] > array[max_value]:
+				max_value = j
+		if i != max_value:
+			temp = array[i]
+			array[i] = array[max_value]
+			array[max_value] = temp
 
 	return array
 
