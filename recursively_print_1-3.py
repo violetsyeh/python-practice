58aed7e059740c48dbb8a3abbf4fea91fed3fbae
Violet Yeh <violetsyeh@gmail.com>
2018-07-23 10:45:06 -0700

    recursively print 1-3

---
+++ recursively_print_1-3.py
@@ -0,0 +1,21 @@
+"""Rescursively print the numbers 1 to 3.
+
+>>> count()
+1
+2
+3
+
+"""
+
+def count(n=1):
+	if n > 3:
+		return
+	print n
+	count(n + 1)
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. YOU'RE TERRIFIC AT THIS!\n")
