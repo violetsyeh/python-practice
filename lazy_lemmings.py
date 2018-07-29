7f5c758879e2dad1a5fffd900534914ff4540990
Violet Yeh <violetsyeh@gmail.com>
2018-07-28 22:53:37 -0700

    made edit

--- lazy_lemmings.py
+++ lazy_lemmings.py
@@ -19,16 +19,36 @@ Find the farthest any single lemming needs to travel for food.
 """
 
 
+# def furthest(num_holes, cafes):
+#     """Find longest distance between a hole and a cafe."""
+
+#     distances = [cafes[0],
+#                  num_holes - cafes[-1] - 1]
+
+#     print distances 
+#     for i in range(1, len(cafes)):
+#         distances.append((cafes[i] - cafes[i - 1]) // 2)
+#     print distances
+#     return max(distances)
+
 def furthest(num_holes, cafes):
     """Find longest distance between a hole and a cafe."""
 
-    distances = [cafes[0],
-                 num_holes - cafes[-1] - 1]
+    # START SOLUTION
 
-    for i in range(1, len(cafes)):
-        distances.append((cafes[i] - cafes[i - 1]) // 2)
+    worst = 0
 
-    return max(distances)
+    for hole in range(num_holes):
+        # Looking at all cafes, find distance to this hole,
+        # and choose the smallest distance.
+
+        dist = [abs(hole - cafe) for cafe in cafes]
+
+        # Keep track of the longest distance we've seen
+
+        worst = max(worst, dist)
+
+    return worst
 
 if __name__ == '__main__':
     import doctest
